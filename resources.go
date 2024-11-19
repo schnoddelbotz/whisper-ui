e472d685f18ac19ea549df7277f6e26cd097b1e9
schnoddelbotz <jan@hacker.ch>
2024-11-20 00:53:52 +0100

    fix CreateTemp usage

--- resources.go
+++ resources.go
@@ -15,7 +15,7 @@ type resources struct {
 func getTempFileName() string {
 	// just interested in a name in $TMP that does not exist.
 	// CreateTemp ensures this ... is there a way to avoid creation to just get name?
-	f, err := os.CreateTemp("", "whisper-ui")
+	f, err := os.CreateTemp("", "whisper-ui-*.wav")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -23,5 +23,5 @@ func getTempFileName() string {
 	if err := f.Close(); err != nil {
 		log.Fatal(err)
 	}
-	return f.Name() + ".wav"
+	return f.Name()
 }
